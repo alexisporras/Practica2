7a1e84d6c1f4799767883a5d51a0ab740e241dd0
alexisporras <real.madrid.alexis@gmail.com>
2018-09-21 22:33:40 -0500

    Variables
    Primer commit

--- Practica2.ino
+++ Practica2.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+//Sensor de movimiento para alarma con sonido
 
+#define bocina 9 //Declaro bocina
+#define sensor 8 //Declaro sensor
+#define led 7 // Declaro led
+
+int mov = 0; //Declaro variable para almacenar movimiento detectado
+
+void setup() {
+  Serial.befin(9600);
+  pinMode(bocina, OUTPUT);
+  pinMode(sensor, INPUT);
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  tone(bocina, 13.75);
+  delay(150);
+  tone(bocina, 9.72);
+  delay(150);
+  tone(bocina, 8.18);
+  delay(150);
+  noTone(bocina);
+  delay(1000);
 }
