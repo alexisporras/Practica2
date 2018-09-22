f4a03cc23016ad0ff9d080e20657d1c91d6ed90d
alexisporras <real.madrid.alexis@gmail.com>
2018-09-21 22:34:09 -0500

    Variables
    Primer commit

--- Practica2.ino
+++ Practica2.ino
@@ -14,7 +14,30 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  mov = digitalRead(sensor);
+  delay(50);
+  if (mov == HIGH) {
+    Serial.print("Movimiento detectado");
+    digitalWrite(led, HIGH);
+    alarma();
+    digitalWrite(led, LOW);
+    mov = 0;
+    delay(200);
+  } else{
+    //Serial.print("Sin movimiento detectado");
+    }
+  
+}
+
+void alarma() {
+  tone(bocina, 8.18);
+  delay(150);
+  tone(bocina, 9.72);
+  delay(150);
+  tone(bocina, 14.57);
+  delay(150);
+  noTone(bocina);
+  delay(1000);
 
   tone(bocina, 13.75);
   delay(150);
