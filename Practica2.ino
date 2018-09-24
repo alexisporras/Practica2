6d1506a9f57d58f1581e2d1188ed071dc7fb8ef9
alexisporras <real.madrid.alexis@gmail.com>
2018-09-23 20:11:25 -0500

    Comentarios
    Agregue comentarios al codigo

--- Practica2.ino
+++ Practica2.ino
@@ -7,23 +7,23 @@
 int mov = 0; //Declaro variable para almacenar movimiento detectado
 void setup() {
   Serial.begin(9600);
-  pinMode(bocina, OUTPUT);
-  pinMode(sensor, INPUT);
-  pinMode(led, OUTPUT);
+  pinMode(bocina, OUTPUT); //Asigno variables con modo de entrada o salida
+  pinMode(sensor, INPUT);  //Asigno variables con modo de entrada o salida
+  pinMode(led, OUTPUT);    //Asigno variables con modo de entrada o salida
 }
 
 void loop() {
-  mov = digitalRead(sensor);
-  delay(100);
+  mov = digitalRead(sensor); //Igualacion de valor del pin con el de variable
+  delay(100); //Tiempo de espera
 
-  if (mov == HIGH) {
+  if (mov == HIGH) { //Condicional de movimiento
     movimiento();
   } else {
     sinMovimiento();
   }
 }
 
-void alarma() {
+void alarma() { //Secuencia de sonido
   tone(bocina, 8.18);
   delay(100);
   tone(bocina, 9.72);
@@ -34,19 +34,19 @@ void alarma() {
   delay(100);
 }
 
-void movimiento() {
-  digitalWrite(led, HIGH);
-  Serial.print("Movimiento");
+void movimiento() { //Secuencia de acciones si se detecta movimiento
+  digitalWrite(led, HIGH); //Encender led
+  Serial.print("Movimiento"); //Comprobar estado en consola
   Serial.print("\n");
-  alarma();
-  digitalWrite(led, LOW);
-  mov = LOW;
-  delay(500);
+  alarma(); //Iniciar secuencia de sonido
+  digitalWrite(led, LOW); //Apagar led
+  mov = LOW; //Rgresar estado de movimiento al original
+  delay(500); //Tiempo de espera
 }
 
-void sinMovimiento() {
-  Serial.print("...");
+void sinMovimiento() { //Secuencia de acciones si no detecta movimiento
+  Serial.print("..."); //Comprobar estado en consola
   Serial.print("\n");
-  digitalWrite(led, LOW);
+  digitalWrite(led, LOW); //Mantener led apagado
   mov = LOW;
 }
