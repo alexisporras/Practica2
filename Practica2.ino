0e40c9367b5c5e7e48fee06648cb7e3f4644f0a1
alexisporras <real.madrid.alexis@gmail.com>
2018-09-23 20:05:13 -0500

    Sensor de movimiento
    Sensibilidad del sensor

--- Practica2.ino
+++ Practica2.ino
@@ -1,11 +1,10 @@
 //Sensor de movimiento para alarma con sonido
 
 #define bocina 6 //Declaro bocina
-#define sensor 9 //Declaro sensor
+#define sensor 2 //Declaro sensor
 #define led 13 // Declaro led
 
-int mov = LOW; //Declaro variable para almacenar movimiento detectado
-
+int mov = 0; //Declaro variable para almacenar movimiento detectado
 void setup() {
   Serial.begin(9600);
   pinMode(bocina, OUTPUT);
@@ -16,37 +15,38 @@ void setup() {
 void loop() {
   mov = digitalRead(sensor);
   delay(100);
-  
+
   if (mov == HIGH) {
-    Serial.print("Movimiento detectado");
-    Serial.print("\n");
-    digitalWrite(led, HIGH);
-    alarma();
-    digitalWrite(led, LOW);
-    mov = LOW;
-    //delay(200);
-  } else{
-    //Serial.print("Sin movimiento detectado");
-    }
-  
+    movimiento();
+  } else {
+    sinMovimiento();
+  }
 }
 
 void alarma() {
-  /*tone(bocina, 8.18);
+  tone(bocina, 8.18);
   delay(100);
   tone(bocina, 9.72);
   delay(100);
   tone(bocina, 14.57);
   delay(100);
   noTone(bocina);
-  delay(100);*/
+  delay(100);
+}
 
-  tone(bocina, 13.75);
-  delay(150);
-  tone(bocina, 9.72);
-  delay(150);
-  tone(bocina, 8.18);
-  delay(150);
-  noTone(bocina);
-  delay(150);
+void movimiento() {
+  digitalWrite(led, HIGH);
+  Serial.print("Movimiento");
+  Serial.print("\n");
+  alarma();
+  digitalWrite(led, LOW);
+  mov = LOW;
+  delay(500);
+}
+
+void sinMovimiento() {
+  Serial.print("...");
+  Serial.print("\n");
+  digitalWrite(led, LOW);
+  mov = LOW;
 }
