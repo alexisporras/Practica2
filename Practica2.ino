d3f8ecedc79efa95fa319210d883fb24ee5d8a71
alexisporras <real.madrid.alexis@gmail.com>
2018-09-26 23:49:28 -0500

    low intensity
    Baja intensidad en consumo de energia

--- Practica2.ino
+++ Practica2.ino
@@ -1,13 +1,13 @@
 /*
- Programa: Alarma
- Objetivo: Implemenatr una alarma para el acceso de puerta. Cuando la puerta se abra,
+  Programa: Alarma
+  Objetivo: Implemenatr una alarma para el acceso de puerta. Cuando la puerta se abra,
            la alarma se debe activar
            Para evitar gastos el consumo de energia debe ser minimo cuando no se abra la puerta,
-           mientras la alarma se active regrese al estado normal y una vez cerrada regrese al 
+           mientras la alarma se active regrese al estado normal y una vez cerrada regrese al
            estado minimo de consumo de energia
-- Alexis Antonio Porras Lobato
+  - Alexis Antonio Porras Lobato
 */
-
+#include "LowPower.h"
 const int pinSensor = 2;
 const int pinLed = 13;
 const int pinBocina = 7;
@@ -17,26 +17,25 @@ void setup() {
   pinMode(pinSensor, INPUT_PULLUP);
   pinMode(pinLed, OUTPUT);
   pinMode(pinBocina, OUTPUT);
-
-  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(pinSensor), alarma, LOW);
+  digitalWrite(pinLed, LOW);
+  digitalWrite(pinBocina, HIGH);
+  digitalWrite(pinSensor, HIGH);
 }
 
 void loop() {
-  int value = digitalRead(pinSensor);
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+}
+void alarma() {
 
-  if (value == LOW) {
-    digitalWrite(pinLed, HIGH);
+  digitalWrite(pinLed, HIGH);
 
-    for (int contador = 0; contador <= 100; contador ++) {
-      tone(pinBocina, 8.18);
-      delay(pinBocina);
+  for(int contador = 0; contador <= 255; contador += 5){
+    analogWrite(pinBocina, 8.18);
+    delay(100);
     }
-    noTone(pinBocina);
+
+  digitalWrite(pinLed, LOW);
 
 
-  } else {
-    digitalWrite(pinLed, LOW);
-  }
-
-  delay(500);
 }
